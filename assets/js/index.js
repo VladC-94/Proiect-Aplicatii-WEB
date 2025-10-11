927bd3bc604e8f54fdedb59b86c329b6ce738b22
VladC-94 <134795092+VladC-94@users.noreply.github.com>
2025-10-11 22:37:14 +0300

    Add files via upload

---
+++ assets/js/index.js
@@ -0,0 +1,20 @@
+// let boing = new Audio("https://www.myinstants.com/media/sounds/boing_lmke36X.mp3");
+// document.querySelector("button").addEventListener("click", () => {
+//   boing.play();
+// });
+
+document.querySelector("#scroll-top").addEventListener("click", () => {
+  window.scrollTo({
+    top: 0, 
+    behavior: "smooth", 
+  })
+})
+
+// document.addEventListener("DOMContentLoaded", () => {
+//   alert("Bine ai venit pe: " + document.title);
+// })
+
+document.querySelector("img").addEventListener("click", function(e) {
+  console.log("X: " + (e.clientX - this.getBoundingClientRect().left));
+  console.log("Y: " + (e.clientY - this.getBoundingClientRect().top));
+})
